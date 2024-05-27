69900eb97b409a4e673a06339e48d1be2aeab851
Maria Kazakova <mariakazakova92@gmail.com>
2024-05-27 17:17:54 +0300

    added method map

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
+import {TaskProps} from "./App";
 
-export const Todolist = () => {
+type TodolistProps = {
+    title: string
+    tasks: TaskProps[]
+    data?: string
+}
+
+export const Todolist = ({title, tasks, data}: TodolistProps) => {
     return (
         <div>
-            <h3>What to learn</h3>
+            <h3>{title}</h3>
             <div>
                 <input/>
                 <button>+</button>
             </div>
             <ul>
-                <li><input type="checkbox" checked={true}/> <span>HTML&CSS</span></li>
-                <li><input type="checkbox" checked={true}/> <span>JS</span></li>
-                <li><input type="checkbox" checked={false}/> <span>React</span></li>
+                {tasks.map(task => {
+                    debugger
+                    return <li key={task.id}><input type="checkbox" checked={true}/><span>{task.title}</span></li>
+                })
+                }
+                {/* <li><input type="checkbox" checked={true}/> <span>{tasks[0].title}</span></li>*/}
+                {/*<li><input type="checkbox" checked={true}/> <span>{tasks[1].title}</span></li>*/}
+                {/*<li><input type="checkbox" checked={false}/> <span>{tasks[2].title}</span></li>*/}
             </ul>
             <div>
                 <button>All</button>
                 <button>Active</button>
                 <button>Completed</button>
             </div>
+            <div>{data}</div>
         </div>
     );
 };
