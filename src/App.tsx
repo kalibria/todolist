39f106def019edeb2a533045c5fe76cb5e4f8ca4
Maria Kazakova <mariakazakova92@gmail.com>
2024-07-01 16:45:05 +0300

    added AppBar from mui

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,13 @@ import {OnOffIndicator} from "../src/onOffIndicator/OnOffIndicator";
 import {v1, v4} from 'uuid';
 import {Todolist} from "./components/todoList/Todolist";
 import {AddItemForm} from "./components/addItemForm/AddItemForm";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 export type TaskProps = {
@@ -120,6 +127,25 @@ const changeTodoListTitle =(todoListId: string, newTitle: string) => {
 }
     return (
         <div className="App">
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <IconButton
+                            size="large"
+                            edge="start"
+                            color="inherit"
+                            aria-label="menu"
+                            sx={{ mr: 2 }}
+                        >
+                            <MenuIcon />
+                        </IconButton>
+                        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                            News
+                        </Typography>
+                        <Button color="inherit">Login</Button>
+                    </Toolbar>
+                </AppBar>
+            </Box>
             <AddItemForm addItem={addTodoList}
             />
             {todoList.map(t => {
